075d1a843d67ede88a528747a1632fe9232a9454
pa-purpura <p.purpura@outlook.fr>
2023-05-19 17:18:11 +0200

    Http client reach external address

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+// https://datahub.io/core/oil-prices/r/brent-day.json
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/GetOil', function (Request $request) {
+
+    $response = Http::get('https://datahub.io/core/oil-prices/r/brent-day.json');
+    dump($response->status());
+    dump($response->ok());
+    $test = $response->json();
+    dump($test[2]);
+    dump($test[2]["Date"]);
 });
