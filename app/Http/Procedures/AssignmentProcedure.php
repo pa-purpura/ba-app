f74d0e31f5613b68094f979651f759ffd7ad1a63
pa-purpura <p.purpura@outlook.fr>
2023-05-20 18:19:36 +0200

    json-rpc service completed

--- app/Http/Procedures/AssignmentProcedure.php
+++ app/Http/Procedures/AssignmentProcedure.php
@@ -6,7 +6,10 @@ namespace App\Http\Procedures;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Sajya\Server\Procedure;
+use Exception;
+
 
 
 class AssignmentProcedure extends Procedure
@@ -30,22 +33,30 @@ class AssignmentProcedure extends Procedure
     {
         $request->validate([
             'startDateISO8601' => 'required|before:endDateISO8601|iso_date',
-            'endDateISO8601' => 'required|after:startDateISO8601|iso_dategit st',
+            'endDateISO8601' => 'required|after:startDateISO8601|iso_date',
         ]);
 
-        $response = Http::get('https://datahub.io/core/oil-prices/r/brent-day.json');
+        try {
+            $response = Http::get('https://datahub.io/core/oil-prices/r/brent-day.json');
 
-        $items = $response->json();
-        $start = $request->input('startDateISO8601');
-        $end = $request->input('endDateISO8601');
-        $prices = [];
+            $items = $response->json();
+            $start = $request->input('startDateISO8601');
+            $end = $request->input('endDateISO8601');
+            $prices = [];
 
-        foreach ($items as $item) {
-            if ($end >= $item["Date"] && $item["Date"] >= $start) {
-                array_push($prices, $item);
+            foreach ($items as $item) {
+                if ($end >= $item["Date"] && $item["Date"] >= $start) {
+                    array_push($prices, $item);
+                }
             }
-        }
 
-        return ['prices' => $prices];
+            return ['prices' => $prices];
+
+        } catch (Exception $e) {
+
+            Log::error($e->getMessage());            
+            throw new Exception($e->getMessage());
+
+        }
     }
 }
